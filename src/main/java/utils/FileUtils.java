d71c31d760801b0ea9722e4c1f2357d44855529c
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2024-08-10 16:39:13 +0300

    Project restructuration
    Maven based project

--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -3,13 +3,9 @@ package utils;
 import com.google.gson.GsonBuilder;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -50,15 +46,6 @@ public class FileUtils {
         }
     }
 
-    public static void extractDirFromInsideJar(Class<?> c, String sourcedir, String target) throws Throwable {
-        byte[] buffer = new byte[4096];
-
-        try (ZipInputStream zis = new ZipInputStream(Objects.requireNonNull(c.getResourceAsStream(sourcedir)))) {
-            unZip(target, buffer, zis);
-        }
-
-    }
-
     public static String getFileContent(String filePath) throws FileNotFoundException {
         StringBuilder content = new StringBuilder();
         File file = new File(filePath);
@@ -70,28 +57,6 @@ public class FileUtils {
         }
 
         return content.toString();
-    }
-
-    public static String getFileContentFromInsideJar(Class<?> c, String filePath) throws IOException {
-        StringBuilder content = new StringBuilder();
-
-        try (Scanner reader = new Scanner(Objects.requireNonNull(c.getResourceAsStream(filePath)))) {
-            while (reader.hasNextLine()) {
-                content.append(reader.nextLine()).append("\n");
-            }
-        }
-
-        return content.toString();
-    }
-
-    public static void copyFileFromInsideJar(Class<?> c, String source, String target) throws Throwable {
-        Path targetPath = Path.of(target);
-
-        try (InputStream stream = c.getResourceAsStream(source)) {
-            assert stream != null;
-            Files.copy(stream, targetPath, StandardCopyOption.REPLACE_EXISTING);
-        }
-
     }
 
     public static void overwriteFileContent(String filePath, String content) throws IOException {
@@ -191,21 +156,6 @@ public class FileUtils {
         return newWord.toString();
     }
 
-    public static String minAll(String s) {
-        char[] characts = s.toCharArray();
-        StringBuilder newWord = new StringBuilder();
-
-        for (char charact : characts) {
-            if (Character.isUpperCase(charact)) {
-                newWord.append(Character.toLowerCase(charact));
-            } else {
-                newWord.append(charact);
-            }
-        }
-
-        return newWord.toString();
-    }
-
     public static <T> T fromJson(Class<T> clazz, String json) {
         GsonBuilder builder = (new GsonBuilder()).registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter());
         builder.registerTypeAdapter(LocalTime.class, new LocalTimeTypeAdapter());
@@ -221,7 +171,4 @@ public class FileUtils {
     }
 
 
-    public static <T> T parse(Class<T> clazz, String value) {
-        return fromJson(clazz, "\"" + value + "\"");
-    }
 }
---
+++ src/test/java/ClassTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class ClassTest {
+    @Test
+    void test() {
+        System.out.println("Hey !");
+    }
+}
