e880aa318998b98df02730f85485c663eec0b03b
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2024-09-02 11:24:45 +0300

    Test

--- src/test/java/templateEngine/TemplateModelRepo.java
+++ src/test/java/templateEngine/TemplateModelRepo.java
@@ -6,56 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class StringTemplateTest {
+public class TemplateModelRepo {
     @Test
-    void strTemplate() {
-        /*String name = "Alice";
-        String greeting = STR."""
-                            Hello, \{name}!
-                            Comment ca va ?
-                            En forme ???
-                                        OK
-        """;
-        // Résultat : "Hello, Alice!"
-        System.out.println(greeting);
-
-        int x = 10;
-        int y = 20;
-        String result = STR."\{x} + \{y} = \{x + y}";
-        System.out.println(result);
-         */
-    }
-
-    @Test
-    void name() {
-        String template = """
-                    [namespace] [package][namespaceStart]
-                    [imports]
-                    [classAnnotations]
-                    public class [classNameMaj] [extends] {
-                        [fields]
-                        [constructors]
-                    }
-                    [namespaceEnd]
-                """;
-
-        template = template.replace("[namespace]", "package")
-                .replace("[namespaceStart]", "")
-                .replace("[namespaceEnd]", "")
-                .replace("[package]", "test.com")
-                .replace("[imports]", "import test;")
-                .replace("[classAnnotations]", "@Table")
-                .replace("[extends]", "extends Table")
-                .replace("[projectNameMin]", "project")
-                .replace("[projectNameMaj]", "Project")
-                .replace("[tableName]", "Project")
-                .replace("[classNameMaj]", "Project");
-
-        System.out.println(template);
-    }
-
-    @Test
-    void templateEngineRender() throws Exception {
+    void templateEngineRenderModel() throws Exception {
 
         String template = """
                 public class ${className} {
