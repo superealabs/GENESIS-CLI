3c754ad233599e082a4a2df93fd38feb95808dca
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2024-09-03 16:12:43 +0300

    Transition -> YAML

--- src/main/java/genesis/config/Constantes.java
+++ src/main/java/genesis/config/Constantes.java
@@ -4,6 +4,8 @@ public class Constantes {
     public static final String DATABASE_JSON = "data_genesis/databases.json";
     public static final String LANGUAGE_JSON = "data_genesis/languages.json";
     public static final String APPLICATION_JSON = "data_genesis/application.json";
+    public static final String APPLICATION_ = "data_genesis/application.json";
+
     public static final String FRAMEWORK_MVC_JSON = "data_genesis/framework-mvc.json";
     public static final String FRAMEWORK_API_JSON = "data_genesis/framework-api.json";
     public static final String DATA_PATH = "data_genesis";
--- src/test/java/templateEngine/yaml/TestParse.java
+++ src/test/java/templateEngine/yaml/TestParse.java
@@ -1,20 +1,25 @@
 package templateEngine.yaml;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import genesis.config.ApplicationType;
 import org.junit.jupiter.api.Test;
+import utils.FileUtils;
 
-import java.io.File;
 import java.io.IOException;
 
 public class TestParse {
     @Test
     void parse() throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-        AppConfig config = objectMapper.readValue(new File("/Users/nomena/STAGE/GENESIS/src/test/java/templateEngine/yaml/config.yaml"), AppConfig.class);
+        AppConfig config = FileUtils.fromYaml(AppConfig.class, "/Users/nomena/STAGE/GENESIS/src/test/java/templateEngine/yaml/config.yaml");
 
         System.out.println("Nom de l'application : " + config.getInfo().getNom());
         System.out.println("Version : " + config.getInfo().getVersion());
         System.out.println("Description :\n" + config.getInfo().getDescription());
+    }
+
+    @Test
+    void application() throws IOException {
+        ApplicationType[] applicationType = FileUtils.fromYaml(ApplicationType[].class, "/Users/nomena/STAGE/GENESIS/data_genesis/yaml/application.yml");
+
+        System.out.println(applicationType.toString());
     }
 }
